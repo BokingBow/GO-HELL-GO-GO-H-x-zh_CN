2e24f8fc73441a5300d7ee64736af25c62799c3c
BokingBow <85831678+BokingBow@users.noreply.github.com>
2026-07-11 13:22:20 +0800

    测试中

--- oneclick.bat
+++ oneclick.bat
@@ -1,5 +1,7 @@
 chcp 65001 >nul
 
+python ue4_localization_toolkit.py import --dir ./Txts_ready --tool "C:/Users/Boking Bow/Exe/PACKCRACK/UE4localizationsTool/UE4localizationsTool.exe" --project-dir ./gohellgo --output ./PatchOutput
+
 :: 把所有文件移入 gohellgo 子目录（PAK 需要此路径结构）
 if not exist ".\PatchOutput\gohellgo" mkdir ".\PatchOutput\gohellgo"
 for /f "delims=" %%i in ('dir /b /a ".\PatchOutput"') do (
@@ -10,5 +12,5 @@ for /f "delims=" %%i in ('dir /b /a ".\PatchOutput"') do (
 
 python ./list.py
 
-del "CC:\Users\Boking Bow\Game\ゴーヘルゴー業 つきおとしてゴー_ver.1.06\gohellgo\Content\Paks\ChineseTest_1_P.pak"
+del "C:\Users\Boking Bow\Game\ゴーヘルゴー業 つきおとしてゴー_ver.1.06\gohellgo\Content\Paks\ChineseTest_1_P.pak"
 "E:\AAA\Epic Games\UE_4.27\Engine\Binaries\Win64\UnrealPak.exe" "C:\Users\Boking Bow\Game\ゴーヘルゴー業 つきおとしてゴー_ver.1.06\gohellgo\Content\Paks\ChineseTest_1_P.pak" -Create="F:\Reason\Origin\GO HELL GO GO 素材\FileList.txt" -compress

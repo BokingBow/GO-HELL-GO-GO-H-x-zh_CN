f5da9cbab49f6aef4c621e74009816777b02cf8a
BokingBow <85831678+BokingBow@users.noreply.github.com>
2026-07-11 13:22:38 +0800

    测试中

---
+++ first.bat
@@ -0,0 +1,3 @@
+chcp 65001 >nul
+
+python ue4_localization_toolkit.py import --dir ./Txts_ready --tool "C:/Users/Boking Bow/Exe/PACKCRACK/UE4localizationsTool/UE4localizationsTool.exe" --project-dir ./gohellgo --output ./PatchOutput
--- oneclick.bat
+++ oneclick.bat
@@ -1,7 +1,5 @@
 chcp 65001 >nul
 
-python ue4_localization_toolkit.py import --dir ./Txts_ready --tool "C:/Users/Boking Bow/Exe/PACKCRACK/UE4localizationsTool/UE4localizationsTool.exe" --project-dir ./gohellgo --output ./PatchOutput
-
 :: 把所有文件移入 gohellgo 子目录（PAK 需要此路径结构）
 if not exist ".\PatchOutput\gohellgo" mkdir ".\PatchOutput\gohellgo"
 for /f "delims=" %%i in ('dir /b /a ".\PatchOutput"') do (

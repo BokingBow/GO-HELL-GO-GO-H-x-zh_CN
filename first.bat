549caeacdf7a3552f9c49d37466c8327b6319ae0
BokingBow <85831678+BokingBow@users.noreply.github.com>
2026-07-12 10:23:15 +0800

    Fix 4

--- first.bat
+++ first.bat
@@ -1,4 +1,4 @@
 chcp 65001 >nul
 
-python ue4_localization_toolkit.py import-changed --dir ./Txts_ready --tool "C:/Users/Boking Bow/Exe/PACKCRACK/UE4localizationsTool/UE4localizationsTool.exe" --project-dir ./gohellgo --output ./PatchOutput/gohellgo
+python ue4_localization_toolkit.py import-changed --dir ./Txts_ready --tool "C:/Users/Boking Bow/Exe/PACKCRACK/UE4localizationsTool/UE4localizationsTool.exe" --project-dir ./gohellgo --output ./PatchOutput/gohellgo --manifest ./manual_maintain.txt
 :: 首次全量请用: python ue4_localization_toolkit.py import ...（同上参数）
